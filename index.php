26b5e83797a46fd9839947fa1276955f0096d7a4
callmenaseef <callmenaseef@gmail.com>
2015-01-28 17:06:32 +0530

    added one photo

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<h1>Hello Naseef</h1>
-<p>how are you man ?</p>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Hello Naseef</h1>
+	<p>how are you man ?</p>
+	<img src="images/avatar4.jpeg" height="300" width="300">
+</body>
+</html>
